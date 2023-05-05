a66716ec55a6028e0f167488cd12021e7ada3baa
reeveboy <reeveboy10@gmail.com>
2023-05-05 10:26:40 +0530

    Header + tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        dark: "#0A192F",
+        light: "#8892B0",
+        primary: "#FF6464",
+        white: "#CCD6F6",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        code: ["Fira Code", "monospace"],
       },
     },
   },
   plugins: [],
-}
+};
