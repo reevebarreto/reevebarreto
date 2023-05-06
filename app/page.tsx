c788c208c0e8d88a5f1570772882017a1b803c68
reeveboy <reeveboy10@gmail.com>
2023-05-07 00:13:06 +0530

    projects + contact sections

--- app/page.tsx
+++ app/page.tsx
@@ -173,6 +173,7 @@ export default function Home() {
             <p className="p-2"></p>
             <div className="h-[1px] grow border border-light_dark"></div>
           </div>
+
           {/* Tasktank */}
           <div className="relative mt-10 flex">
             <a
@@ -190,7 +191,7 @@ export default function Home() {
                 height={400}
               />
             </a>
-            <div className="absolute right-0 top-0 flex flex-col items-end">
+            <div className="absolute inset-0 flex flex-col items-end">
               <span className="font-code text-sm text-primary">
                 Featured Project
               </span>
@@ -206,7 +207,7 @@ export default function Home() {
               </a>
               <p className="p-2"></p>
               <div className="z-20 flex rounded bg-dark_light p-6 shadow-md transition-all hover:shadow-2xl">
-                <span className="w-full max-w-sm text-sm text-light ">
+                <span className="w-full max-w-sm text-right text-sm text-light">
                   Tasktank is a productivity application which can be used by
                   individuals or companies to create, assign tasks track their
                   progress.
@@ -221,7 +222,7 @@ export default function Home() {
                 <span>Prisma</span>
                 <p className="p-2"></p>
                 <span>tRPC</span>
-                <p className="p-1"></p>
+                <p className="p-2"></p>
                 <span>TailwindCSS</span>
               </div>
               <p className="p-2"></p>
@@ -250,7 +251,192 @@ export default function Home() {
               </div>
             </div>
           </div>
+
+          <p className="p-12"></p>
+
+          {/* Anomus */}
+          <div className="relative mt-10 flex flex-row-reverse">
+            <a
+              href="https://anomus.vercel.app/"
+              target="_blank"
+              className="relative z-10 before:absolute before:inset-0 before:block before:rounded before:bg-primary/40 before:duration-300 before:hover:bg-primary/0"
+            >
+              <Image
+                src={
+                  "https://user-images.githubusercontent.com/81756283/235133137-ceef3b57-134b-4685-8a92-b7a3f0527ea3.png"
+                }
+                alt="anomus homepage"
+                className="w-full max-w-lg rounded object-cover"
+                width={600}
+                height={400}
+              />
+            </a>
+            <div className="absolute inset-0 flex flex-col items-start">
+              <span className="font-code text-sm text-primary">
+                Featured Project
+              </span>
+              <p className="p-1"></p>
+              <a
+                href="https://anomus.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="text-2xl font-medium text-white transition-all duration-300 hover:text-primary">
+                  Anomus
+                </span>
+              </a>
+              <p className="p-2"></p>
+              <div className="z-20 flex rounded bg-dark_light p-6 shadow-md transition-all hover:shadow-2xl">
+                <span className="w-full max-w-sm text-sm text-light ">
+                  Anomus is a place to share anonymous messages about any topics
+                  you want. I had first started this project to learn more about
+                  the intricasies of NextJS and Prisma. And it turned out to be
+                  pretty useful.
+                </span>
+              </div>
+              <p className="p-2"></p>
+              <div className="flex font-code text-xs text-light">
+                <p className="p-2"></p>
+                <span>NextJS</span>
+                <p className="p-2"></p>
+                <span>Prisma</span>
+                <p className="p-2"></p>
+                <span>TailwindCSS</span>
+                <p className="p-2"></p>
+                <span>tRPC</span>
+              </div>
+              <p className="p-2"></p>
+              <div className="flex">
+                <a
+                  href="https://github.com/reeveboy/anomus"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    className="text-lg text-white transition-all duration-300 hover:text-primary"
+                  />
+                </a>
+                <p className="p-2"></p>
+                <a
+                  href="https://anomus.vercel.app"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon
+                    icon={faArrowUpRightFromSquare}
+                    className="text-lg text-white transition-all duration-300 hover:text-primary"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <p className="p-12"></p>
+
+          {/* Citrus */}
+          <div className="relative mt-10 flex">
+            <a
+              href="https://github.com/reeveboy/citrus-web"
+              target="_blank"
+              className="relative z-10 before:absolute before:inset-0 before:block before:rounded before:bg-primary/40 before:duration-300 before:hover:bg-primary/0"
+            >
+              <Image
+                src={
+                  "https://user-images.githubusercontent.com/81756283/134859134-c03cde34-633b-49e3-be00-35f25c57ac6d.png"
+                }
+                alt="tasktank dashboard"
+                className="w-full max-w-lg rounded object-cover"
+                width={600}
+                height={400}
+              />
+            </a>
+            <div className="absolute inset-0 flex flex-col items-end">
+              <span className="font-code text-sm text-primary">
+                Featured Project
+              </span>
+              <p className="p-1"></p>
+              <a
+                href="https://tasktank.vercel.app/signin"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="text-2xl font-medium text-white transition-all duration-300 hover:text-primary">
+                  Citrus
+                </span>
+              </a>
+              <p className="p-2"></p>
+              <div className="z-20 flex rounded bg-dark_light p-6 shadow-md transition-all hover:shadow-2xl">
+                <span className="w-full max-w-sm text-right text-sm text-light">
+                  Citrus was the first fullstack application I had ever built. I
+                  used all the knowledge and skills I had gained from watching
+                  youtube videos and put it all together. It was a great
+                  learning experience and I&apos;m proud of what I had built.
+                  <p className="p-2"></p>
+                  Citrus is a POS Service for restaurants to track and manage
+                  their bills and inventory.
+                </span>
+              </div>
+              <p className="p-2"></p>
+              <div className="flex font-code text-xs text-light">
+                <span>React</span>
+                <p className="p-2"></p>
+                <span>GraphQl</span>
+                <p className="p-2"></p>
+                <span>Typescript</span>
+                <p className="p-2"></p>
+                <span>Apollo</span>
+                <p className="p-2"></p>
+                <span>Express</span>
+              </div>
+              <p className="p-2"></p>
+              <div className="flex">
+                <a
+                  href="https://github.com/reeveboy/citrus-web"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    className="text-lg text-white transition-all duration-300 hover:text-primary"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
+
+        <p className="p-16"></p>
+
+        {/* Contact */}
+        <div className="flex h-screen flex-col items-center justify-center text-center">
+          <span className="font-code text-sm text-primary">
+            04. So What&apos;s Next?
+          </span>
+          <p className="p-3"></p>
+          <span className="text-5xl font-semibold text-white">
+            Get In Touch
+          </span>
+          <p className="p-3"></p>
+          <span className="text-md w-full max-w-md text-light">
+            Since I have just completed my exams and I&apos;ll be officially
+            graduating soon, I&apos;m open for any opportunities that come my
+            way. Shoot me a DM or an email and I&apos;ll get back to you as soon
+            as I can.
+          </span>
+          <p className="p-3"></p>
+          <a
+            href="mailto:reeveboy10@gmail.com"
+            className="rounded border border-primary px-6 py-4 font-code text-sm text-primary"
+          >
+            Say Hi!
+          </a>
+        </div>
+        <p className="p-4"></p>
+        <div className="text-center font-code text-xs text-light">
+          - by Reeve Barreto
+        </div>
+        <p className="p-4"></p>
       </main>
 
       {/* Side Socials */}
@@ -259,24 +445,27 @@ export default function Home() {
           href="https://github.com/reeveboy"
           target="_blank"
           rel="noopener noreferrer"
+          className="text-xl text-light transition-all duration-300 hover:-translate-y-1 hover:text-primary"
         >
-          <FontAwesomeIcon icon={faGithub} className="text-xl text-light" />
+          <FontAwesomeIcon icon={faGithub} />
         </a>
         <p className="p-2"></p>
         <a
           href="https://www.instagram.com/reeeevve/"
           target="_blank"
           rel="noopener noreferrer"
+          className="text-xl text-light transition-all duration-300 hover:-translate-y-1 hover:text-primary"
         >
-          <FontAwesomeIcon icon={faInstagram} className="text-xl text-light" />
+          <FontAwesomeIcon icon={faInstagram} />
         </a>
         <p className="p-2"></p>
         <a
           href="https://www.linkedin.com/in/reeve-barreto-779620220"
           target="_blank"
           rel="noopener noreferrer"
+          className="text-xl text-light transition-all duration-300 hover:-translate-y-1 hover:text-primary"
         >
-          <FontAwesomeIcon icon={faLinkedinIn} className="text-xl text-light" />
+          <FontAwesomeIcon icon={faLinkedinIn} />
         </a>
         <p className="p-2"></p>
         <div className="h-20 w-[1px] border border-light"></div>
@@ -284,7 +473,7 @@ export default function Home() {
       <div className="fixed bottom-0 right-[5%]  flex translate-x-1/2 flex-col items-center">
         <a
           href="mailto:reeveboy10@gmail.com"
-          className="-translate-y-20 rotate-90 font-code text-sm text-light"
+          className="-translate-y-20 rotate-90 font-code text-sm text-light transition-all duration-300 hover:-translate-y-[5.5rem] hover:text-primary"
         >
           reeveboy10@gmail.com
         </a>
